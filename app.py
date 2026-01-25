9f840ae8e98e843cbff856d2ceb5d69b89029c93
marcootavio2008 <marcootavio2008@gmail.com>
2026-01-24 22:51:44 -0300

    Fix duplicate SQLAlchemy initialization
    Removed duplicate SQLAlchemy initialization.

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
 db = SQLAlchemy(app)
 
 class Registro(db.Model):
@@ -27,7 +24,6 @@ class Registro(db.Model):
 
 with app.app_context():
     db.create_all()
-
 
 @app.route("/")
 def index():
